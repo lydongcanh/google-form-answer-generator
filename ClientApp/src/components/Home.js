dccb74927dd6af9ddbd66b90c1e21bdb6afd8c24
Lý Đông Cảnh <lydongcanh@gmail.com>
2021-02-21 12:00:49 +0700

    Add methods to fetch form data from url.

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,26 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import { InputGroup, InputGroupAddon, Button, Input } from "reactstrap";
+import Axios from "axios";
 
-export class Home extends Component {
-  static displayName = Home.name;
+export default function Home() {
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
+  const [formUrl, setFormUrl] = useState("https://docs.google.com/forms/u/0/d/e/1FAIpQLSeQ1Srh0b7jOD38nf1YjzlObyw8YwKN5urj-aLCFymaG2YVEQ/formResponse");
+  const [numberOfPages, setNumberOfPages] = useState(1);
+
+  async function getFormData() {
+    const response = await Axios.get(`api/googleForms?formUrl=${formUrl}&numberOfPages=${numberOfPages}`);
+    const funcString = "function getData() { " + response.data + " return FB_PUBLIC_LOAD_DATA_; } getData()";
+    const result = eval(funcString);
+    
+    const dataList = result[1][1];
+    console.log(dataList);
+    for(const data of dataList) {
+      const lastItem = data[data.length - 1];
+      if (Array.isArray(lastItem)) {
+        console.log("Question", {
+          "question": data[1],
+          "answers": lastItem[0][1],
+          "entry.id": lastItem[0][0],
+        });
+        continue;
+      }
+
+      console.log("Session", data[1]);
+    }
   }
+
+  return (
+    <InputGroup>
+      <InputGroupAddon addonType="prepend">
+        <Input
+          placeholder="Number of pages"
+          type="number"
+          onChange={(e) => setNumberOfPages(e.target.value)}
+          value={numberOfPages}
+          min={0}
+          max={100}
+          step={1}
+        />
+      </InputGroupAddon>
+      <Input
+        placeholder="Form url..."
+        onChange={(e) => setFormUrl(e.target.value)}
+        value={formUrl}
+      />
+      <InputGroupAddon addonType="append">
+        <Button color="secondary" onClick={getFormData}>Get Data</Button>
+      </InputGroupAddon>
+    </InputGroup>
+  );
 }
--- Controllers/GoogleFormsController.cs
+++ Controllers/GoogleFormsController.cs
@@ -1,7 +1,11 @@
 ﻿using System.Threading.Tasks;
+using System.Net.Http;
+using System.Linq;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using GoogleFormAnswerGenerator.Models.GoogleForm;
 using GoogleFormAnswerGenerator.Services;
+using HtmlAgilityPack;
 
 namespace GoogleFormAnswerGenerator.Controllers
 {
@@ -21,6 +25,35 @@ namespace GoogleFormAnswerGenerator.Controllers
 
             var result = await googleForms.SubmitAsync();
             return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get(string formUrl, int numberOfPages = 0)
+        {
+            /// Add multiple pages support.
+            var parameters = new Dictionary<string, string>();
+            var pageHistory = "0";
+            for(int i = 1; i <= numberOfPages; i++)
+                pageHistory += "," + i;
+            parameters.Add("pageHistory", pageHistory);
+
+            /// Try to post a fault request to the form to get questions & answers data.
+            var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync(formUrl, new FormUrlEncodedContent(parameters));
+            var rawHtml = await response.Content.ReadAsStringAsync();
+
+            /// Get data in <script> in script node. 
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(rawHtml);
+            var script = htmlDocument
+                .DocumentNode
+                .Descendants()
+                .Where(n => n.Name.Equals("script"))
+                .Select(sn => sn.InnerText)
+                .Where(it => it.Contains("FB_PUBLIC_LOAD_DATA_"))
+                .FirstOrDefault();
+
+            return Ok(script);
         }
     }
 }
